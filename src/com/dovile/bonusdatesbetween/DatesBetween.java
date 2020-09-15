b496b3117686e00151556eded55fe47d17385f4a
Doreina <barkauskaite.dovile@gmail.com>
2020-09-15 13:46:06 +0300

    create project

---
+++ src/com/dovile/bonusdatesbetween/DatesBetween.java
@@ -0,0 +1,12 @@
+package com.dovile.bonusdatesbetween;
+
+public class DatesBetween {
+
+    static void printBonusDatesBetween(int fromYear, int toYear){
+
+        System.out.println(fromYear+" "+" "+toYear);
+    }
+    public static void main(String[] args) {
+        printBonusDatesBetween(2010,2015);
+    }
+}
